93b1e5b50277da1eefa4da9d04d82c5f9a304419
Kosisochukwu Nuelsage <Realitynuel@gmail.com>
2023-04-06 10:06:34 -0700

    final commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 (function () {
 
     let screen = document.querySelector(".screen");
@@ -11,97 +12,22 @@
             screen.value += value;
         })
     });
+
+   
     equal.addEventListener('click', function(e){
         if(screen.value === '') {
             screen.value = "Please enter a value";
         }
         else {
-            try {
-                let result = evaluate(screen.value);
-                screen.value = result;
-            } catch (e) {
-                screen.value = "Error";
-            }
+            const result = eval(screen.value);
+            screen.value = result
         }
     });
-
-    function evaluate(str) {
-        let result;
-        try {
-          result = Function('return (' + str + ')')();
-        } catch (e) {
-          result = "Error";
-        }
-        return result;
-      }
     clear.addEventListener('click', function(e){
-        screen.value = ""
-
-    })
-
+        screen.value = "";
+    });
 
 })();
 
 
-
-// (function () {
-
-//     let screen = document.querySelector(".screen");
-//     let buttons = document.querySelectorAll(".btn");
-//     let equal = document.querySelector(".btn-equal");
-//     let clear = document.querySelector(".btn-clear");
-
-//     buttons.forEach(function(button){
-//         button.addEventListener('click', function(e){
-//             let value = e.target.dataset.num;
-//             screen.value += value;
-//         })
-//     });
-
-//     equal.addEventListener('click', function(e){
-//         let result;
-//         try {
-//             result = eval(screen.value);
-//         } catch (e) {
-//            screen = result;
-//         }
-//         screen.value = result;
-//     });
-
-//     clear.addEventListener('click', function(e){
-//         screen.value = "";
-//     });
-
-// })();
-
-
-// (function () {
-
-//     let screen = document.querySelector(".screen");
-//     let buttons = document.querySelectorAll(".btn");
-//     let equal = document.querySelector(".btn-equal");
-//     let clear = document.querySelector(".btn-clear");
-
-//     buttons.forEach(function(button){
-//         button.addEventListener('click', function(e){
-//             let value = e.target.dataset.num;
-//             screen.value += value;
-//         })
-//     });
-
-   
-//     equal.addEventListener('click', function(e){
-//         if(screen.value === '') {
-//             screen.value = "Please";
-//         }
-//         else {
-//             const result = eval(screen.value);
-//             screen.value = result
-//         }
-//     });
-//     clear.addEventListener('click', function(e){
-//         screen.value = "";
-//     });
-
-// })();
 
